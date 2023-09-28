92b42d64e5d0527a36761872e491dad73eaf5cb4
Manuel Morales <mf.moraless1@uniandes.edu.co>
2023-09-27 19:58:57 -0500

    add scroll action

---
+++ src/test/java/utilities/ScrollAction.java
@@ -0,0 +1,18 @@
+package utilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import pages.BasePage;
+
+public class ScrollAction extends BasePage {
+    public ScrollAction(AppiumDriver<MobileElement> driver) {
+        super(driver);
+        // TODO Auto-generated constructor stub
+    }
+
+    public void scrollElement() {
+        driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollToBeginning(10000)"));
+        driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(100000)"));
+    }
+}
