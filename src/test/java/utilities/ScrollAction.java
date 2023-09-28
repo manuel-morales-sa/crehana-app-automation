9a9754901678783e792c1519e0d8e82144a95077
Manuel Morales <mf.moraless1@uniandes.edu.co>
2023-09-27 19:52:20 -0500

    sove conflicts 2

--- src/test/java/utilities/ScrollAction.java
+++ src/test/java/utilities/ScrollAction.java
@@ -1,13 +1,11 @@
-package Utilities;
+package utilities;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import pages.BasePage;
 
-
 public class ScrollAction extends BasePage {
-
     public ScrollAction(AppiumDriver<MobileElement> driver) {
         super(driver);
         // TODO Auto-generated constructor stub
@@ -15,15 +13,8 @@ public class ScrollAction extends BasePage {
 
     public void scrollElement() {
         driver.findElement(MobileBy.AndroidUIAutomator(
-<<<<<<< HEAD
-                "new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(10000000)"));
-        driver.findElement(MobileBy.AndroidUIAutomator(
-                "new UiScrollable(new UiSelector().scrollable(true)).scrollToBeginning(10000000)"));
-    }
-
-=======
                 "new UiScrollable(new UiSelector().scrollable(true)).scrollToBeginning(10000)"));
         driver.findElement(MobileBy.AndroidUIAutomator(
                 "new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(100000)"));
->>>>>>> develop
     }
+}
